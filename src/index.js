2a31e35adf4a00cda4d0fe729fcd97e6149d8470
anil kadam <anilkadam210@gmail.com>
2020-05-13 22:47:07 +0530

    Retrieving location from geolocation api

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        err => console.log(err)
+    );
     return (
         <div>
             <SeasonDisplay />
