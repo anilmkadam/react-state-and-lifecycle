6dd2e7fd8f73649614ae4345dfe5afb49297ff69
anil kadam <anilkadam210@gmail.com>
2020-05-13 23:11:13 +0530

    Refactored functional component to class based componet

--- src/index.js
+++ src/index.js
@@ -2,18 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        err => console.log(err.message)
-    );
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            err => console.log(err.message)
+        );
     return (
         <div>
             <SeasonDisplay />
         </div>
     );
+    } 
 }
-
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
