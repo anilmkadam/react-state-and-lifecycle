ec226f50d9c3569a4b86abfbe8ccc783acc11709
anil kadam <anilkadam210@gmail.com>
2020-05-19 20:06:10 +0530

    Added state object for storing lat and long info

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {lat: null}
+    }
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
@@ -10,7 +15,7 @@ class App extends React.Component {
         );
     return (
         <div>
-            <SeasonDisplay />
+            Latitude: 
         </div>
     );
     } 
