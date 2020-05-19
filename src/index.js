fb49a5dde6b3cca1ebe4b0ccb1b9b543b7efed6a
anil kadam <anilkadam210@gmail.com>
2020-05-19 20:57:08 +0530

    Handling Error - handled user location error if user not give location permission

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: 40 };
+    this.state = { lat: null, errMessage: "" };
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
-      (err) => console.log(err.message)
+      (err) => this.setState({ errMessage: err.message })
     );
   }
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errMessage}
+      </div>
+    );
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
