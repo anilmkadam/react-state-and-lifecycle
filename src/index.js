7adc1b71f8d38007dffd4d009f77f2e27b2c4923
anil kadam <anilkadam210@gmail.com>
2020-05-22 22:49:59 +0530

    Refactoerd Code - moved api call in ComponentDidMount for initial data loading

--- src/index.js
+++ src/index.js
@@ -8,15 +8,13 @@ class App extends React.Component {
     super(props);
 
     this.state = { lat: null, errMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errMessage: err.message })
     );
-  }
-
-  componentDidMount() {
-    console.log("component was rendered to the screen...");
   }
   componentDidUpdate() {
     console.log("component was just updated - it rerendered...");
@@ -29,6 +27,7 @@ class App extends React.Component {
     return (
       <div>
         <Loader type='Rings' color='#00BFFF' height={100} width={100} />
+        Loading....
       </div>
     );
   }
