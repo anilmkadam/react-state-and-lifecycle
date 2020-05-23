b3a87e80256b60c2691eb9b2a37661f50c1d0dfb
anil kadam <anilkadam210@gmail.com>
2020-05-23 15:31:22 +0530

    Refactoring code - Extracting options from seasonConfig obj

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,23 +1,32 @@
 import React from "react";
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach.",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Burr, it is chilly.",
+    iconName: "snowflake",
+  },
+};
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? "Summer" : "Winter";
+    return lat > 0 ? "summer" : "winter";
   } else {
-    return lat > 0 ? "Winter" : "Summer";
+    return lat > 0 ? "winter" : "summer";
   }
 };
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const seasonDisplayText =
-    season === "Winter" ? "Burr, it is chilly." : "Lets hit the beach";
-  const icon = season === "Winter" ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season];
+
   return (
     <div>
-      <i className={`${icon} icon`} />
-      <h1>{seasonDisplayText}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
